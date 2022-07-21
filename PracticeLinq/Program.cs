d87bde21861e2a7c630f7a0ebc4f051b74f320b7
MikeNichols1371 <mnich13711@gmail.com>
2022-07-21 10:45:44 -0400

    Finished Exercise 2

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,7 @@
+﻿var videoGameNames = new List<string>()
+{"Grand Theft Auto","Assassins Creed", "Ghost of Tsumashu", "Far Cry"};
+
+var orderByNameLength = videoGameNames.OrderBy(name => name.Length).ToList();
+orderByNameLength.ForEach(name => Console.WriteLine(name));
+
+            
